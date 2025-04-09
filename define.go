d0c3e305e1dd116e9bcb1bc32e0054be1da98f0b
YannianyYi <yiyannian@chivalrous-tech.com>
2025-04-09 14:20:49 +0800

    refactor: 定义通用的切片和映射类型
    - 新增多个切片类型，如 SInt、SString 等，用于不同数据类型的集合
    - 新增多个映射类型，如 MSInt、MSString 等，用于键值对存储
    - 这些类型定义在 simple包中，提供了更明确的类型标识，有助于提高代码的可读性和安全性

---
+++ define.go
@@ -0,0 +1,36 @@
+package simple
+
+type (
+	Slices  []any
+	SInt    []int
+	SInt8   []int8
+	SInt16  []int16
+	SInt32  []int32
+	SInt64  []int64
+	SUint   []uint
+	SUint8  []uint8
+	SUint16 []uint16
+	SUint32 []uint32
+	SUint64 []uint64
+	SString []string
+	SBool   []bool
+	SByte   []byte
+)
+
+type (
+	Map      map[string]any
+	MapAny   map[string]any
+	MSString map[string]string
+	MSInt    map[string]int
+	MSInt8   map[string]int8
+	MSInt16  map[string]int16
+	MSInt32  map[string]int32
+	MSInt64  map[string]int64
+	MSUint   map[string]uint
+	MSUint8  map[string]uint8
+	MSUint16 map[string]uint16
+	MSUint32 map[string]uint32
+	MSUint64 map[string]uint64
+	MSBool   map[string]bool
+	MSByte   map[string]byte
+)
