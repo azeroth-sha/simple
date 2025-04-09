fe92f050fa76901d1d487a64232f7ce214741dac
YannianyYi <yiyannian@chivalrous-tech.com>
2025-04-09 14:08:42 +0800

    refactor(simple): 优化接口定义和文档注释
    -为 interface.go 中的接口添加文档注释，明确每个接口的用途
    - 为 logger.go 中的 Logger 接口添加详细文档注释，包括每个方法的功能和参数说明
    - 优化代码结构，增加空行和注释，提高代码可读性和维护性

--- interface.go
+++ interface.go
@@ -1,61 +1,76 @@
 package simple
 
+// String 定义了返回字符串表示的接口
 type String interface {
-	String() string
+	String() string // 返回对象的字符串表示
 }
 
+// Bytes 定义了返回字节切片的接口
 type Bytes interface {
-	Bytes() []byte
+	Bytes() []byte // 返回对象的字节切片表示
 }
 
+// Int 定义了返回 int 类型值的接口
 type Int interface {
-	Int() int
+	Int() int // 返回对象的 int 类型值
 }
 
+// Int8 定义了返回 int8 类型值的接口
 type Int8 interface {
-	Int8() int8
+	Int8() int8 // 返回对象的 int8 类型值
 }
 
+// Int16 定义了返回 int16 类型值的接口
 type Int16 interface {
-	Int16() int16
+	Int16() int16 // 返回对象的 int16 类型值
 }
 
+// Int32 定义了返回 int32 类型值的接口
 type Int32 interface {
-	Int32() int32
+	Int32() int32 // 返回对象的 int32 类型值
 }
 
+// Int64 定义了返回 int64 类型值的接口
 type Int64 interface {
-	Int64() int64
+	Int64() int64 // 返回对象的 int64 类型值
 }
 
+// Uint 定义了返回 uint 类型值的接口
 type Uint interface {
-	Uint() uint
+	Uint() uint // 返回对象的 uint 类型值
 }
 
+// Uint8 定义了返回 uint8 类型值的接口
 type Uint8 interface {
-	Uint8() uint8
+	Uint8() uint8 // 返回对象的 uint8 类型值
 }
 
+// Uint16 定义了返回 uint16 类型值的接口
 type Uint16 interface {
-	Uint16() uint16
+	Uint16() uint16 // 返回对象的 uint16 类型值
 }
 
+// Uint32 定义了返回 uint32 类型值的接口
 type Uint32 interface {
-	Uint32() uint32
+	Uint32() uint32 // 返回对象的 uint32 类型值
 }
 
+// Uint64 定义了返回 uint64 类型值的接口
 type Uint64 interface {
-	Uint64() uint64
+	Uint64() uint64 // 返回对象的 uint64 类型值
 }
 
+// Float32 定义了返回 float32 类型值的接口
 type Float32 interface {
-	Float32() float32
+	Float32() float32 // 返回对象的 float32 类型值
 }
 
+// Float64 定义了返回 float64 类型值的接口
 type Float64 interface {
-	Float64() float64
+	Float64() float64 // 返回对象的 float64 类型值
 }
 
+// Bool 定义了返回 bool 类型值的接口
 type Bool interface {
-	Bool() bool
+	Bool() bool // 返回对象的 bool 类型值
 }
--- logger.go
+++ logger.go
@@ -1,15 +1,34 @@
 package simple
 
+// Logger 定义了日志记录器的接口，支持不同级别的日志记录和格式化输出
 type Logger interface {
+	// Debug 记录调试级别的日志，参数为任意类型的值
 	Debug(v ...interface{})
+
+	// Info 记录信息级别的日志，参数为任意类型的值
 	Info(v ...interface{})
+
+	// Warn 记录警告级别的日志，参数为任意类型的值
 	Warn(v ...interface{})
+
+	// Error 记录错误级别的日志，参数为任意类型的值
 	Error(v ...interface{})
+
+	// Fatal 记录致命错误级别的日志，参数为任意类型的值，通常会导致程序退出
 	Fatal(v ...interface{})
 
+	// Debugf 记录调试级别的日志，支持格式化字符串和参数
 	Debugf(format string, v ...interface{})
+
+	// Infof 记录信息级别的日志，支持格式化字符串和参数
 	Infof(format string, v ...interface{})
+
+	// Warnf 记录警告级别的日志，支持格式化字符串和参数
 	Warnf(format string, v ...interface{})
+
+	// Errorf 记录错误级别的日志，支持格式化字符串和参数
 	Errorf(format string, v ...interface{})
+
+	// Fatalf 记录致命错误级别的日志，支持格式化字符串和参数，通常会导致程序退出
 	Fatalf(format string, v ...interface{})
 }
